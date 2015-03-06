cb809f00edca72d54fc744fb0b635bc9ad271a01
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-03-05 19:54:50 -0500

    styled index it works let's keep it that way ;)

--- wp-content/themes/theme-hackeryou/index.php
+++ wp-content/themes/theme-hackeryou/index.php
@@ -2,17 +2,29 @@
 <?php get_header(); ?>
 
 <div class="main">
-  <div class="containerIndex">
+  <div class="container">
 
-    <div class="contentIndex">
-	<div class="wrapperIndex">
+    <div class="content">
+	<div class="wrapperIndex clearfix">
+	    
 	    <?php while ( have_posts() ) : the_post(); ?>
-	    	<div class="postTitle">
-	    		<a href="<?php the_permalink(); ?>">
-	    			<?php echo get_the_title(); ?>
-	    		</a>
-	    		
-	    	</div>	
+	    	<div class="postWrapper clearfix">
+		    	<div class="postTitle">
+		    		<div class="titleText">
+			    		<a href="<?php the_permalink(); ?>">
+			    			<?php echo get_the_title(); ?>
+			    		</a>
+		    		</div>
+		    	</div>	
+		    	<div class="postImage clearfix">
+		    		<?php $thumb_id = get_post_thumbnail_id();
+		    		$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+		    		$thumb_url = $thumb_url_array[0]; ?>
+		    		<a href="<?php the_permalink(); ?>">
+		    		 	<div class="image" style="background-image: url('<?php echo $thumb_url ?>')"></div>
+		    		</a> 
+		    	</div>
+		    </div>	<!-- end.postWrapper -->	
 	    <?php endwhile; // End the loop. Whew. ?>	
 	    	
     	</div> <!-- end .wrapperIndex -->
